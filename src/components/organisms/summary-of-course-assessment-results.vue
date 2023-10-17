1a8e262bf1a33b4f5286c0ad1829f8b3339192a0
lazlanrafar <lazlanrafar@gmail.com>
2023-10-17 08:04:50 +0700

    update

--- src/components/organisms/summary-of-course-assessment-results.vue
+++ src/components/organisms/summary-of-course-assessment-results.vue
@@ -3,6 +3,41 @@
     <div class="card-header fw-medium">Summary of Course Assessment Results</div>
 
     <div class="card-body">
+      <div class="row">
+        <div class="col-12" v-if="assessment">
+          <div class="card shadow-none border">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <TableCustom
+                    :items="[
+                      { label: 'Study Program', value: assessment.course.program_studi.title },
+                      { label: 'Course Code', value: assessment.course.code },
+                      { label: 'Course Title', value: assessment.course.title },
+                      { label: 'Teacher NIP', value: assessment.teacher_nip },
+                      { label: 'Teacher Name', value: assessment.teacher_name },
+                    ]"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <TableCustom
+                    :items="[
+                      { label: 'Semester', value: assessment.semester },
+                      { label: 'Academic Year', value: assessment.academic_year },
+                      { label: 'Class', value: assessment.class },
+                      {
+                        label: 'Proficiency Level',
+                        value: `${assessment.proficiency_level.level} (${assessment.proficiency_level.description})`,
+                      },
+                    ]"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="table-responsive" v-if="summary_of_course_assessment_results.category">
         <table class="table table-bordered fs-12">
           <thead class="table-success">
@@ -43,26 +78,52 @@
       <div class="row">
         <div class="col-md-6">
           <div class="card shadow-none border">
-            <div class="card-header">SA - RE202 Student Outcomes Attainment</div>
+            <div class="card-header">SA - {{ assessment.course.code }} Student Outcomes Attainment</div>
             <div class="card-body">
               <GChart
                 type="ColumnChart"
                 :data="summary_of_course_assessment_results.category_chart"
-                :options="chartOptions"
                 v-if="summary_of_course_assessment_results.category_chart"
+                :options="chartColumnOptions"
               />
             </div>
           </div>
         </div>
         <div class="col-md-6">
           <div class="card shadow-none border">
-            <div class="card-header">SA - RE202 Student Outcomes Attainment</div>
+            <div class="card-header">SA - {{ assessment.course.code }} Attainment</div>
+            <div class="card-body">
+              <GChart
+                type="ColumnChart"
+                :data="summary_of_course_assessment_results.category_chart_target"
+                v-if="summary_of_course_assessment_results.category_chart_target"
+                :options="chartTargetOptions(summary_of_course_assessment_results.category_chart_target[0].length - 2)"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card shadow-none border">
+            <div class="card-header">SA - {{ assessment.course.code }} Proficiency Level Attainment</div>
             <div class="card-body">
               <GChart
                 type="ColumnChart"
                 :data="summary_of_course_assessment_results.proficiency_level_chart"
-                :options="chartOptions"
                 v-if="summary_of_course_assessment_results.proficiency_level_chart"
+                :options="chartColumnOptions"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card shadow-none border">
+            <div class="card-header">SA - {{ assessment.course.code }} Level Proficiency Level Attainment</div>
+            <div class="card-body">
+              <GChart
+                type="ColumnChart"
+                :data="summary_of_course_assessment_results.proficiency_level_chart_target"
+                v-if="summary_of_course_assessment_results.proficiency_level_chart_target"
+                :options="chartTargetOptions(1)"
               />
             </div>
           </div>
@@ -79,14 +140,25 @@ export default {
   name: "SummaryOfCourseAssessmentResults",
   components: {
     GChart,
+    TableCustom: () => import("@/components/molecules/table-custom.vue"),
   },
   data() {
     return {
-      chartOptions: {
+      chartColumnOptions: {
         isStacked: "percent",
         height: 350,
         legend: { position: "bottom", maxLines: 5 },
         chartArea: { width: "85%" },
+      },
+      chartTargetOptions: (target) => {
+        return {
+          isStacked: true,
+          height: 350,
+          legend: { position: "bottom", maxLines: 5 },
+          chartArea: { width: "85%" },
+          seriesType: "bars",
+          series: { [target]: { type: "line" } },
+        };
       },
     };
   },
@@ -99,6 +171,9 @@ export default {
     },
     summary_of_course_assessment_results() {
       return this.$store.state.assessment.summary_of_course_assessment_results;
+    },
+    assessment() {
+      return this.$store.state.assessment.summary_of_course_assessment_results.assessment;
     },
   },
   mounted() {
