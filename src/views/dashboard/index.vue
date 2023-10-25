89a94bb54eb9953e6124fcbae381d93f13ea4f68
lazlanrafar <lazlanrafar@gmail.com>
2023-10-26 00:12:18 +0700

    update dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,7 +4,14 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Dashboard</h1>
+            <h1>
+              <img
+                src="https://gifdb.com/images/high/waving-hand-hi-emoticon-5ogrlew1e9zb3p5m.gif"
+                alt="hand"
+                width="40"
+              />
+              Welcome
+            </h1>
           </div>
         </div>
       </div>
@@ -12,7 +19,47 @@
 
     <div class="content">
       <div class="container-fluid">
-        <div class="row">
+        <div class="card shadow-none border">
+          <div class="card-body">
+            <h1 class="mb-5 h5">Aplikasi Pengukuran Ketercapaian Capaian Pembelajaran</h1>
+            <p>
+              Pengukuran Capaian Pembelajaran (CP) adalah proses penting dalam pendidikan untuk menilai sejauh mana
+              peserta didik mencapai tujuan pembelajaran yang telah ditetapkan. CP yang berbasis outcome lebih
+              menekankan pada hasil yang dapat diukur daripada hanya memperhatikan proses pembelajaran. Pentingnya
+              pengukuran CP termasuk memungkinkan meningkatkan kualitas pembelajaran, dan membantu peserta didik
+              memahami pencapaian mereka.
+            </p>
+            <p>
+              Keunggulan utama dari pendekatan berbasis outcome adalah objektivitas. Hasil yang diukur dapat
+              diekspresikan dalam data numerik, yang kurang rentan terhadap penilaian subjektif atau bias. Selain itu,
+              outcome pembelajaran yang jelas dan terukur memfasilitasi perbandingan dengan tujuan yang telah
+              ditetapkan, sehingga memudahkan penilaian kemajuan.
+            </p>
+
+            <p>Source Code</p>
+            <ul>
+              <li>
+                Frontend :
+                <a
+                  href="https://github.com/lazlanrafar/course-assessment-polibatam-web"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >https://github.com/lazlanrafar/course-assessment-polibatam-web
+                </a>
+              </li>
+              <li>
+                Backend :
+                <a
+                  href="https://github.com/lazlanrafar/course-assessment-polibatam-be"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >https://github.com/lazlanrafar/course-assessment-polibatam-be
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- <div class="row">
           <div class="col-md-3 col-sm-6 col-12">
             <div class="info-box shadow-none bg-gradient-info">
               <span class="info-box-icon"><i class="far fa-bookmark"></i></span>
@@ -68,7 +115,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </layout-app>
